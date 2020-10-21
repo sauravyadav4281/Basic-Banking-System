e360633379b8b8230dcfcc271e89c4a7cd737bc2
sauravyadav4281 <70005578+sauravyadav4281@users.noreply.github.com>
2020-10-21 23:10:54 +0530

    Add files via upload

---
+++ footer.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<head>
+<title>Untitle document</title>
+</head>
+<body>
+<table width ="1316" bgcolor="red" style=" color:white;bottom:0px;font-size:20px;">
+<tr>
+<td class="f" align="center" >The Sparks Foundation Graduate Rotational Internship Program project by Saurav Yadav </td>
+</tr>
+</table>
+</html>
---
+++ index.php
@@ -0,0 +1,27 @@
+<html>
+<head>
+<title> WELCOME TO SAURAV BANKING </title>
+<link rel="stylesheet" type="text/css" href="headerbtn.css">
+</head>
+<body style = "background-image : url('bankpic.jpg'); width=1920px; height=1080;">
+<div allign ="center" style="top:0px">
+<table width="1330">
+<tr>
+<td><h1 style="font-family : georgia; text-color:green ">WELCOME TO SAURAV BANKING </h1> </td>
+</tr>
+<table width="1316" align = "center" class = "t">
+<tr>
+<td style="text-align :center "> <button class= "btn">Home </button></td>
+<td style="text-align :center "> <a href="customerdetails.php" target="frame"><button class = "btn2">customers</button></a></td>
+<td style="text-align :center "> <a href="selectuser.php" target="frame"><button class = "btn2">Transaction</button></a></td>
+<td style="text-align :center "> <a href="history.php" target="frame"><button class = "btn2">Transaction History</button></a></td>
+</tr>
+</table>
+</table>
+</div>
+<div>
+<img src="bankpic.jpg" alt="Forest" height="500" style="opacity:0.5">
+</div>
+<?php include 'footer.php';?>
+</body>
+</html>
